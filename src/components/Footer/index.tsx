24fb0fc9abf8d7f82b7a688bc0046f883f35f790
Pedro Rabis <rabis.pedro@gmail.com>
2022-02-15 11:43:13 -0300

    :sparkles: Footer Component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+import './styles.css';
+
+const Footer: React.FC = () => {
+	return (
+		<>
+			<footer>
+				<p>Desenvolvido por Carlito Alves, Gustavo Spindola e Pedro Rabis</p>
+			</footer>
+		</>
+	);
+}
+
+export default Footer;
