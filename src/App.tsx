19b830d274cde78ac75e9755720b2cc82a09d691
Pedro Rabis <rabis.pedro@gmail.com>
2022-02-17 10:34:22 -0300

    :construction: Integração com api

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,8 @@ function App() {
 	return (
 		<>
 			<Header />
-			<PokemonDetails />
+
+			<PokemonDetails id='57'/>
 			<Footer />
 		</>
 	);
--- src/components/PokemonDetails/index.tsx
+++ src/components/PokemonDetails/index.tsx
@@ -1,345 +1,154 @@
 import './styles.css'
-// import Api from 'services/Api/index';
+import Api from 'services/Api/index';
+import { useEffect, useState } from 'react';
 
+interface PokemonDetailsProps {
+	id: string;
+}
 
-const PokemonDetails: React.FC = () => {
-	const detail = 'Pokémon are the creatures that inhabit the world of the Pokémon games. They can be caught using Pokéballs and trained by battling with other Pokémon. Each Pokémon belongs to a specific species but may take on a variant which makes it differ from other Pokémon of the same species, such as base stats, available abilities and typings.';
+// interface pokemonInfo {
+// 	id: number;
+// 	name: string;
+// 	base_experience: number;
+// 	height: number;
+// 	weight: number;
+// 	abilities: pokemonAbilities[];
+// 	moves: pokemonMoves[];
+// 	types: pokemonTypes[];
+// 	sprites: string;
+// }
 
-	// const pokemonApi = Api.get('https://pokeapi.co/api/v2/pokemon/1');
+// interface pokemonSprites {
+// 	versions: pokemonGames[];
+// }
 
-	
+// interface pokemonGames {
+// 	generations: pokemonGameGeneration;
+// }
 
-	const pokemon = {
-		"id": 35,
-		"name": "clefairy",
-		"base_experience": 113,
-		"height": 6,
-		"is_default": true,
-		"order": 56,
-		"weight": 75,
-		"abilities": [
-			{
-				"is_hidden": true,
-				"slot": 3,
-				"ability": {
-					"name": "friend-guard",
-					"url": "https://pokeapi.co/api/v2/ability/132/"
-				}
-			}
-		],
-		"forms": [
-			{
-				"name": "clefairy",
-				"url": "https://pokeapi.co/api/v2/pokemon-form/35/"
-			}
-		],
-		"game_indices": [
-			{
-				"game_index": 35,
-				"version": {
-					"name": "white-2",
-					"url": "https://pokeapi.co/api/v2/version/22/"
-				}
-			}
-		],
-		"held_items": [
-			{
-				"item": {
-					"name": "moon-stone",
-					"url": "https://pokeapi.co/api/v2/item/81/"
-				},
-				"version_details": [
-					{
-						"rarity": 5,
-						"version": {
-							"name": "ruby",
-							"url": "https://pokeapi.co/api/v2/version/7/"
-						}
-					}
-				]
-			}
-		],
-		"location_area_encounters": "/api/v2/pokemon/35/encounters",
-		"moves": [
-			{
-				"move": {
-					"name": "pound",
-					"url": "https://pokeapi.co/api/v2/move/1/"
-				},
-				"version_group_details": [
-					{
-						"level_learned_at": 1,
-						"version_group": {
-							"name": "red-blue",
-							"url": "https://pokeapi.co/api/v2/version-group/1/"
-						},
-						"move_learn_method": {
-							"name": "level-up",
-							"url": "https://pokeapi.co/api/v2/move-learn-method/1/"
-						}
-					}
-				]
-			}
-		],
-		"species": {
-			"name": "clefairy",
-			"url": "https://pokeapi.co/api/v2/pokemon-species/35/"
-		},
-		"sprites": {
-			"back_default": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/35.png",
-			"back_female": null,
-			"back_shiny": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/35.png",
-			"back_shiny_female": null,
-			"front_default": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/35.png",
-			"front_female": null,
-			"front_shiny": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/35.png",
-			"front_shiny_female": null,
-			"other": {
-				"dream_world": {
-					"front_default": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/35.svg",
-					"front_female": null
-				},
-				"home": {
-					"front_default": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/35.png",
-					"front_female": null,
-					"front_shiny": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/shiny/35.png",
-					"front_shiny_female": null
-				},
-				"official-artwork": {
-					"front_default": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/35.png"
-				}
-			},
-			"versions": {
-				"generation-i": {
-					"red-blue": {
-						"back_default": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-i/red-blue/back/35.png",
-						"back_gray": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-i/red-blue/back/gray/35.png",
-						"front_default": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-i/red-blue/35.png",
-						"front_gray": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-i/red-blue/gray/35.png"
-					},
-					"yellow": {
-						"back_default": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-i/yellow/back/35.png",
-						"back_gray": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-i/yellow/back/gray/35.png",
-						"front_default": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-i/yellow/35.png",
-						"front_gray": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-i/yellow/gray/35.png"
-					}
-				},
-				"generation-ii": {
-					"crystal": {
-						"back_default": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-ii/crystal/back/35.png",
-						"back_shiny": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-ii/crystal/back/shiny/35.png",
-						"front_default": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-ii/crystal/35.png",
-						"front_shiny": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-ii/crystal/shiny/35.png"
-					},
-					"gold": {
-						"back_default": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-ii/gold/back/35.png",
-						"back_shiny": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-ii/gold/back/shiny/35.png",
-						"front_default": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-ii/gold/35.png",
-						"front_shiny": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-ii/gold/shiny/35.png"
-					},
-					"silver": {
-						"back_default": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-ii/silver/back/35.png",
-						"back_shiny": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-ii/silver/back/shiny/35.png",
-						"front_default": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-ii/silver/35.png",
-						"front_shiny": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-ii/silver/shiny/35.png"
-					}
-				},
-				"generation-iii": {
-					"emerald": {
-						"front_default": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-iii/emerald/35.png",
-						"front_shiny": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-iii/emerald/shiny/35.png"
-					},
-					"firered-leafgreen": {
-						"back_default": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-iii/firered-leafgreen/back/35.png",
-						"back_shiny": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-iii/firered-leafgreen/back/shiny/35.png",
-						"front_default": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-iii/firered-leafgreen/35.png",
-						"front_shiny": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-iii/firered-leafgreen/shiny/35.png"
-					},
-					"ruby-sapphire": {
-						"back_default": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-iii/ruby-sapphire/back/35.png",
-						"back_shiny": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-iii/ruby-sapphire/back/shiny/35.png",
-						"front_default": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-iii/ruby-sapphire/35.png",
-						"front_shiny": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-iii/ruby-sapphire/shiny/35.png"
-					}
-				},
-				"generation-iv": {
-					"diamond-pearl": {
-						"back_default": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-iv/diamond-pearl/back/35.png",
-						"back_female": null,
-						"back_shiny": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-iv/diamond-pearl/back/shiny/35.png",
-						"back_shiny_female": null,
-						"front_default": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-iv/diamond-pearl/35.png",
-						"front_female": null,
-						"front_shiny": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-iv/diamond-pearl/shiny/35.png",
-						"front_shiny_female": null
-					},
-					"heartgold-soulsilver": {
-						"back_default": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-iv/heartgold-soulsilver/back/35.png",
-						"back_female": null,
-						"back_shiny": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-iv/heartgold-soulsilver/back/shiny/35.png",
-						"back_shiny_female": null,
-						"front_default": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-iv/heartgold-soulsilver/35.png",
-						"front_female": null,
-						"front_shiny": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-iv/heartgold-soulsilver/shiny/35.png",
-						"front_shiny_female": null
-					},
-					"platinum": {
-						"back_default": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-iv/platinum/back/35.png",
-						"back_female": null,
-						"back_shiny": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-iv/platinum/back/shiny/35.png",
-						"back_shiny_female": null,
-						"front_default": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-iv/platinum/35.png",
-						"front_female": null,
-						"front_shiny": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-iv/platinum/shiny/35.png",
-						"front_shiny_female": null
-					}
-				},
-				"generation-v": {
-					"black-white": {
-						"animated": {
-							"back_default": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/back/35.gif",
-							"back_female": null,
-							"back_shiny": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/back/shiny/35.gif",
-							"back_shiny_female": null,
-							"front_default": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/35.gif",
-							"front_female": null,
-							"front_shiny": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/shiny/35.gif",
-							"front_shiny_female": null
-						},
-						"back_default": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/back/35.png",
-						"back_female": null,
-						"back_shiny": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/back/shiny/35.png",
-						"back_shiny_female": null,
-						"front_default": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/35.png",
-						"front_female": null,
-						"front_shiny": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/shiny/35.png",
-						"front_shiny_female": null
-					}
-				},
-				"generation-vi": {
-					"omegaruby-alphasapphire": {
-						"front_default": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-vi/omegaruby-alphasapphire/35.png",
-						"front_female": null,
-						"front_shiny": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-vi/omegaruby-alphasapphire/shiny/35.png",
-						"front_shiny_female": null
-					},
-					"x-y": {
-						"front_default": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-vi/x-y/35.png",
-						"front_female": null,
-						"front_shiny": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-vi/x-y/shiny/35.png",
-						"front_shiny_female": null
-					}
-				},
-				"generation-vii": {
-					"icons": {
-						"front_default": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-vii/icons/35.png",
-						"front_female": null
-					},
-					"ultra-sun-ultra-moon": {
-						"front_default": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-vii/ultra-sun-ultra-moon/35.png",
-						"front_female": null,
-						"front_shiny": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-vii/ultra-sun-ultra-moon/shiny/35.png",
-						"front_shiny_female": null
-					}
-				},
-				"generation-viii": {
-					"icons": {
-						"front_default": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-viii/icons/35.png",
-						"front_female": null
-					}
-				}
-			}
-		},
-		"stats": [
-			{
-				"base_stat": 35,
-				"effort": 0,
-				"stat": {
-					"name": "speed",
-					"url": "https://pokeapi.co/api/v2/stat/6/"
-				}
-			}
-		],
-		"types": [
-			{
-				"slot": 1,
-				"type": {
-					"name": "fairy",
-					"url": "https://pokeapi.co/api/v2/type/18/"
-				}
-			}
-		],
-		"past_types": [
-			{
-				"generation": {
-					"name": "generation-v",
-					"url": "https://pokeapi.co/api/v2/generation/5/"
-				},
-				"types": [
-					{
-						"slot": 1,
-						"type": {
-							"name": "normal",
-							"url": "https://pokeapi.co/api/v2/type/1/"
-						}
-					}
-				]
-			}
-		]
+// interface pokemonGameGeneration {
+// 	name: string;
+// }
+
+// interface pokemonGifs {
+// 	name: string;
+// }
+
+// interface pokemonAbilities {
+// 	ability: pokemonName;
+// }
+
+// interface pokemonMoves {
+// 	move: pokemonName;
+// }
+
+// interface pokemonTypes {
+// 	type: pokemonName;
+// }
+
+// interface pokemonName {
+// 	name: string;
+// }
+
+const PokemonDetails: React.FC<PokemonDetailsProps> = ({ id }) => {
+	const url = '/pokemon/';
+	const [currentPokemon, setCurrentPokemon] = useState({});
+
+	useEffect(() => {
+		(async () => {
+			
+			const poke = await fetchPokemon(id);
+			console.log("Poke: ", poke);
+		}) ()
+	}, []);
+
+	let pokemon = {};
+	const fetchPokemon = async (id: string) => {
+		await Api.get(url + id)
+			.then((response: any) => {
+				console.log("Pokemon: ", response.data);
+				setCurrentPokemon(response.data);
+				console.log("CurrentPokemon: ", currentPokemon);
+				pokemon = response.data;
+				console.log("Pokemon definitivo: ", pokemon);
+				return response.data;
+			})
+			.catch((err: any) => {
+				console.error(err);
+			})
+			// return pokemon;
 	}
-	
+
 	return (
 		<>
 			<div className='pokemon-details-container'>
+				<img src={currentPokemon.sprites.versions['generation-v']['black-white'].animated.front_default} alt={`${pokemon.name}`} />
+				<h2>{currentPokemon.name}</h2>
+
 				
-				<img src={pokemon.sprites.versions['generation-v']['black-white'].animated.front_default} alt={`${pokemon.name} image`} />
+				{/* <img src={pokemon.sprites.versions['generation-v']['black-white'].animated.front_default} alt={`${pokemon.name}`} />
 
-				<div className='pokemon-id-container'>
-					<h2>#{pokemon.id}</h2>
-					<h1>{pokemon.name}</h1>
+				<div className='pokemon-info-container'>
+					<p>#{pokemon.id}</p>
+					<h2>{pokemon.name}</h2>
 				</div>
 
-				<div className='types-container'>
-					<h2>Types:</h2>
-						{pokemon.types.map(type => {
-							return (
-								<div className='pokemon-types-container'>
-									<p>{type.type.name}</p>
-								</div>
-							);
-						})}
+				<div className='pokemon-info-container'>
+					<h2>Height:</h2>
+					<p>{pokemon.height / 10}m</p>
 				</div>
 
-				<div className='abilities-container'>
+				<div className='pokemon-info-container'>
+					<h2>Weight:</h2>
+					<p>{pokemon.weight / 10}kg</p>
+				</div>
+
+				<div className='pokemon-info-container'>
+					<h2>Base XP:</h2>
+					<p>{pokemon.base_experience}</p>
+				</div>
+			
+				<div className='category-container'>
 					<h2>Abilities:</h2>
-						{pokemon.abilities.map(ability => {
+					<div className='category-grid'>
+
+						{pokemon.abilities?.map(ability => {
 							return (
-								<div className='pokemon-abilities-container'>
+								<div className='pokemon-category-container'>
 									<p>{ability.ability.name}</p>
 								</div>
 							);
 						})}
+					</div>
 				</div>
 
-				<h3>Moves:</h3>
-					{pokemon.moves.map(move => {
-						return (
-							<div className='pokemon-moves-container'>
-								<p>{move.move.name}</p>
-							</div>
-						);
-					})}
+				<div className='category-container'>
+					<h2>Moves:</h2>
+					<div className='category-grid'>
 
-				<div className='pokemon-informations-container'>
-					<h2>Height:</h2>
-					<h2>{pokemon.height / 10}m</h2>
-
-					<h2>Weight:</h2>
-					<h2>{pokemon.weight / 10}kg</h2>
-
-					<h2>Base XP:</h2>
-					<h2>{pokemon.base_experience}</h2>
+						{pokemon.moves.map(move => {
+							return (
+								<div className='pokemon-category-container'>
+									<p>{move.move.name}</p>
+								</div>
+							);
+						})}
+					</div>
 				</div>
+
+				<div className='category-container'>
+					<h2>Types:</h2>
+					<div className='category-grid'>
+							
+						{pokemon.types.map(type => {
+							return (
+								<div className='pokemon-category-container'>
+									<p>{type.type.name}</p>
+								</div>
+							);
+						})}
+					</div>
+				</div> */}
 
 			</div>
 		</>
